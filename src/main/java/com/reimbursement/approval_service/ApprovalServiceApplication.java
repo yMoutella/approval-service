5499f265253b0b4ca782e7d4b82c85a8c4b9e434
Yure Moutella <ymoutella@gmail.com>
2025-06-19 16:27:28 -0300

    inital creation

---
+++ src/main/java/com/reimbursement/approval_service/ApprovalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reimbursement.approval_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalServiceApplication.class, args);
+	}
+
+}
